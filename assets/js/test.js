0ef1f8fb6bb62656d097a0aba39154a70fe59c16
Chris Curran <chriscurran8@yahoo.com>
2020-10-01 21:57:59 +0000

    add function to not get a repeated tile

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,21 +1,45 @@
 function selectNumber() {
   var numArr = [];
   var num = 0;
-  var index = 1;
+  var count = 1;
+  var duplicate = 0;
 
-  while (index < 10) {
-    num = Math.floor(Math.random() * 16) + 1;
-    if (numArr === []) {
+  while (count <= 10) {
+    num = Math.floor(Math.random() * 12) + 1;
+    if (numArr.length === 0) {
+      console.log("empty array getting 1st element");
       numArr.push(num);
     } else {
-      if (num != numArr[index]) {
-        numArr.push(num);
-        index++;
+      debugger;
+      for (var index = 0; index < numArr.length; index++) {
+        if (numArr[index] === num) {
+          duplicate = 1;
+          break;
+        } else {
+          numArr.push(num);
+        }
       }
+    }
+    if (duplicate) {
+      duplicate = 0;
+    } else {
+      count++;
     }
   }
   console.log(numArr);
 }
 
-
 selectNumber();
+
+// var arr1 = [];
+// var arr = [];
+// arr[0] = 7;
+// var x = 7;
+// if(arr[0] === x){
+//     console.log("A Match!");
+// }
+// if(arr1 = []){
+//   console.log("Blank");
+// } else {
+//   console.log("FULL");
+// }
